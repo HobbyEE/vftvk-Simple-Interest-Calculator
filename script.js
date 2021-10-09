6528a5785953a92a24de20e45389702091721fc1
HobbyEE <nkalousis@gmail.com>
2021-10-09 10:28:45 +0300

    save in order to sync in all devices

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+let sliderValue = document.getElementById('sliderValue');
+let rate = document.getElementById('rate');
+sliderValue.innerHTML = Number(rate.value).toFixed(2) + ' %';
+
+rate.addEventListener('mousemove', () => {
+    sliderValue.innerHTML = Number(rate.value).toFixed(2) + ' %';
+})
+
+rate.addEventListener('click', () => {
+    sliderValue.innerHTML = Number(rate.value).toFixed(2) + ' %';
+})
+
 function compute()
 {
     p = document.getElementById("principal").value;
